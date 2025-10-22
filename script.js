31204c91b15d811329a31787338ce5d15cadee29
harsh-accio <rajputharsh9897@gmail.com>
2025-10-22 22:14:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+
+async function getMessage(){
+const text = document.getElementById('text').value;
+const time = document.getElementById('delay').value;
+const btn = document.getElementById('btn');
+const output = document.getElementById('output');
+	await setTimeout(()=>{
+		output.innerText=text;
+	},time);
+	
+}
